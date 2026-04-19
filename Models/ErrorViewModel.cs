70c38e7f0eb0fc17b862cd9d1329f279648bf5cc
Yaser Shaikh <syaser327@gmail.com>
2026-04-19 20:12:39 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpensesTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
