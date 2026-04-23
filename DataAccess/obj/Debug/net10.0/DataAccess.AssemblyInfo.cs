483ace316fe8d2ad5ed96c3262445328d93a7592
Yaser Shaikh <syaser327@gmail.com>
2026-04-23 14:25:10 +0530

    Updated

--- DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7128794054b1fddf7a8e38dce1be3bac4484a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
